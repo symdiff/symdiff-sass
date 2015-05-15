020498440fff916c84154a30d450d47e1796a742
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-15 12:52:46 +0200

    update test case for indented syntax

--- test/symdiff-sass.test.js
+++ test/symdiff-sass.test.js
@@ -1,35 +1,74 @@
 var extract = require('../index');
 
 describe('symdiff-sass', function() {
-    it('should work with an empty sass', function() {
-        var result = extract('');
-        expect(result.length).to.equal(0);
+    describe('SCSS', function() {
+        it('should work with an empty scss', function() {
+            var result = extract('');
+            expect(result.length).to.equal(0);
+        });
+
+        it('should work with invalid scss', function() {
+            var result = extract('this is not the scss you are looking for');
+            expect(result.length).to.equal(0);
+        });
+
+        it('should extract a class', function() {
+            var testSCSS = '.grid { display: flex; }',
+                result = extract(testSCSS);
+
+            expect(result.length).to.equal(1);
+            expect(result[0]).to.equal('grid');
+        });
+
+        it('should extract nested classes', function() {
+            var testSCSS = '.grid { display: flex; .grid-col { flex: 0 0 auto; }}',
+                result = extract(testSCSS);
+
+            expect(result.length).to.equal(2);
+        });
+
+        it('should extract nothing when there are no classes', function() {
+            var testSCSS = '#grid { display: flex; }',
+                result = extract(testSCSS);
+
+            expect(result.length).to.equal(0);
+        });
     });
 
-    it('should work with invalid sass', function() {
-        var result = extract('this is not the sass you are looking for');
-        expect(result.length).to.equal(0);
-    });
+    describe('SASS', function() {
+        it('should work with an empty sass', function() {
+            var result = extract('');
+            expect(result.length).to.equal(0);
+        });
 
-    it('should extract a class', function() {
-        var testSASS = '.grid { display: flex; }',
-            result = extract(testSASS);
+        it('should work with invalid sass', function() {
+            var result = extract('this is not the sass you are looking for');
+            expect(result.length).to.equal(0);
+        });
 
-        expect(result.length).to.equal(1);
-        expect(result[0]).to.equal('grid');
-    });
+        it('should extract a class', function() {
+            var testSASS = '.grid\n  display: flex',
+                result = extract(testSASS);
 
-    it('should extract nested classes', function() {
-        var testSASS = '.grid { display: flex; .grid-col { flex: 0 0 auto; }}',
-            result = extract(testSASS);
+            expect(result.length).to.equal(1);
+            expect(result[0]).to.equal('grid');
+        });
 
-        expect(result.length).to.equal(2);
-    });
+        it('should extract nested classes', function() {
+            var testSASS = '.grid\n'+
+                           '  display: flex\n'+
+                           '  .grid-col\n'+
+                           '    flex: 0 0 auto',
+                result = extract(testSASS);
 
-    it('should extract nothing when there are no classes', function() {
-        var testSASS = '#grid { display: flex; }',
-            result = extract(testSASS);
+            expect(result.length).to.equal(2);
+        });
 
-        expect(result.length).to.equal(0);
+        it('should extract nothing when there are no classes', function() {
+            var testSASS = '#grid\n  display: flex',
+                result = extract(testSASS);
+
+            expect(result.length).to.equal(0);
+        });
     });
 });
